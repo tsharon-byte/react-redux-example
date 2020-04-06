221e92bd6c1b0d407ae984c1f9321cd93dc078ac
tsharon-byte <sharonova_t@inbox.ru>
2020-04-06 11:50:26 +0300

    Updates

---
+++ precache-manifest.a99e3ffcbf8dfd0ab98717645a73e9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c72829c16dc63081abb9e9212c3e3c5",
+    "url": "/react-redux-example/index.html"
+  },
+  {
+    "revision": "81b4ff19ea6dbd1dfae4",
+    "url": "/react-redux-example/static/css/main.971e67e1.chunk.css"
+  },
+  {
+    "revision": "a276b3370f0f2ea0698c",
+    "url": "/react-redux-example/static/js/2.abfb2d51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-example/static/js/2.abfb2d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b4ff19ea6dbd1dfae4",
+    "url": "/react-redux-example/static/js/main.a2d89f1a.chunk.js"
+  },
+  {
+    "revision": "80d8fd6c9ff6c972fb1c",
+    "url": "/react-redux-example/static/js/runtime-main.933a12a2.js"
+  }
+]);
